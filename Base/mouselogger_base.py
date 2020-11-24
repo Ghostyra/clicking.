500bc9e07d034d6d0eea621341addf3d526bb282
Ghostyra <marsfilin@yandex.ru>
2020-11-24 23:38:47 +0900

    Del venv; change pandas to simple file-reader

--- Base/mouselogger_base.py
+++ Base/mouselogger_base.py
@@ -1,6 +1,5 @@
 import logging
 from typing import Optional
-
 from pynput.mouse import Button
 from pynput import mouse
 
@@ -9,19 +8,19 @@ class MouseLoggerBase(object):
     def __init__(self):
         self.buttons_dict = {Button.left: "left.Click", Button.right: "right.Click"}
         self.logging = logging
-        self.logging.basicConfig(filename="mouse_log.csv", level=logging.INFO, format="%(asctime)s.%(message)s")
+        self.logging.basicConfig(filename="mouse_log.csv", level=logging.INFO, format="%(asctime)s|%(message)s")
         self.listener()
 
     def get_foreground_window_title(self) -> Optional[str]:
         raise NotImplemented
 
     def on_move(self, x, y):
-        self.logging.info("{0}.{1}.None.None.Move.".format(x, y) + self.get_foreground_window_title())
+        self.logging.info("{0}|{1}|None|None|Move|".format(x, y) + self.get_foreground_window_title())
 
     def on_click(self, x, y, button, pressed):
         if button == Button.middle:
             return False
-        self.logging.info("{0}.{1}.{2}.{3}.".format(x, y, self.buttons_dict[button], (
+        self.logging.info("{0}|{1}|{2}|{3}|".format(x, y, self.buttons_dict[button], (
             "pressed" if pressed else "released")) + self.get_foreground_window_title())
 
     def listener(self):
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import pandas as pd
 import os
 from screeninfo import get_monitors
 from datetime import datetime
 from sys import platform
 
 
-def foo():
-    columns = ["date", "x", "y", "press_release", "button_type", "action_type", "active_window"]
-    df = pd.read_csv("mouse_log.csv", sep=".", names=columns, encoding="cp1252")
-
+def save_csv():
     dt = datetime.now().replace(second=0, microsecond=0).strftime('%d.%m.%Y %H.%M')
-
     monitors = get_monitors()
-
     if not os.path.isdir("mouse_data"):
         os.mkdir("mouse_data")
 
     save_path = (r"mouse_data/" + dt +
                  " ({0} x {1})".format(monitors[0].width, monitors[0].height) +
                  ".csv")
-    df.to_csv(save_path)
+
+    header = r"date|x|y|press_release|button_type|action_type|git active_window"
+    with open(save_path, "w") as file:
+        with open("mouse_log.csv", "r+") as f:
+            data = f.read()
+            file.write(header + "\n" + data)
+
     print(os.path.abspath(save_path))
 
 
@@ -33,7 +33,7 @@ class Main:
         else:
             from Services.linux_mouse_logger import LinuxMouseLogger
             LinuxMouseLogger()
-        foo()
+        save_csv()
 
 
 if __name__ == "__main__":
